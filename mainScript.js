17c60201d3c8ebeee9f533f216a04981a0492ced
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2023-04-21 19:21:58 +0800

    Update mainScript.js
    Modified with the aid of ChatGPT.

--- mainScript.js
+++ mainScript.js
@@ -4,22 +4,15 @@ canvas.height = window.innerHeight;
 document.body.appendChild(canvas);
 
 const context = canvas.getContext("2d");
-const radius = 80;
-let x = canvas.width / 2;
-let y = canvas.height / 2;
-let dx = 5;
-let dy = 5;
+const radius = 60;
+var x = canvas.width / 2;
+var y = canvas.height / 2;
+var isDragging = false;
+var offsetX = 0;
+var offsetY = 0;
 
 function animateCircle() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    x += dx;
-    y += dy;
-    if (x + radius >= canvas.width || x - radius <= 0) {
-        dx = -dx;
-    }
-    if (y + radius >= canvas.height || y - radius <= 0) {
-        dy = -dy;
-    }
     context.beginPath();
     context.arc(x, y, radius, 0, Math.PI * 2);
     context.fillStyle = "blue";
@@ -28,3 +21,24 @@ function animateCircle() {
 }
 
 animateCircle();
+
+canvas.addEventListener("mousedown", function(event) {
+    const rect = canvas.getBoundingClientRect();
+    offsetX = event.clientX - rect.left;
+    offsetY = event.clientY - rect.top;
+    if (Math.sqrt(Math.pow(offsetX - x, 2) + Math.pow(offsetY - y, 2)) <= radius) {
+        isDragging = true;
+    }
+});
+
+canvas.addEventListener("mousemove", function(event) {
+    if (isDragging) {
+        const rect = canvas.getBoundingClientRect();
+        x = event.clientX - rect.left - offsetX;
+        y = event.clientY - rect.top - offsetY;
+    }
+});
+
+canvas.addEventListener("mouseup", function(event) {
+    isDragging = false;
+});
