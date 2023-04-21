34d60d1b910f474c22b6c047b2ca30df05438860
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2023-04-21 20:47:44 +0800

    Update mainScript.js
    Now can drag more circles.

--- mainScript.js
+++ mainScript.js
@@ -5,44 +5,66 @@ document.body.appendChild(canvas);
 canvas.style.zIndex = 3;
 
 const context = canvas.getContext("2d");
-const radius = 40;
-var x = canvas.width / 2;
-var y = canvas.height / 2;
-var isDragging = false;
-var newX;
-var newY;
-
-function showCircle() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.beginPath();
-    context.arc(x, y, radius, 0, Math.PI * 2);
-    context.fillStyle = "green";
-    context.fill();
-    requestAnimationFrame(showCircle);
+const circleRadius = 30;
+const circleNumber = 5;
+var oriX = new Array(circleNumber);
+var oriY = new Array(circleNumber);
+for (var i = 0; i != circleNumber; i++) {
+    oriX[i] = 80 * (i + 1);
+    oriY[i] = 80 * (i + 1);
 }
 
-showCircle();
 
-function updateCircle(mousedown = false) {
+function showCircle(i) {
+    context.beginPath();
+    context.arc(oriX[i], oriY[i], circleRadius, 0, Math.PI * 2);
+    context.fillStyle = "green";
+    context.fill();
+    context.strokeStyle = "red";
+    context.lineWidth = 2;
+    context.stroke();
+}
+
+var animationId;
+function showCircles() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    for (var i = 0; i != circleNumber; i++) {
+        showCircle(i);
+    }
+}
+
+animationId = requestAnimationFrame(showCircles); // to cancel, use cancelAnimationFrame(animationID);
+
+var isDragging = false;
+var whichDragging;
+var newX;
+var newY;
+var offsetX;
+var offsetY;
+
+canvas.addEventListener("mousedown", function(event) {
     const rect = canvas.getBoundingClientRect();
     newX = event.clientX - rect.left;
     newY = event.clientY - rect.top;
-    if (mousedown && ((newX - x, 2) * (newX - x, 2) + (newY - y, 2) * (newY - y, 2) <= radius * radius)) {
-        isDragging = true;
+    for (var i = 0; !isDragging && i != circleNumber; i++) {
+        if ((newX - oriX[i]) * (newX - oriX[i]) + (newY - oriY[i]) * (newY - oriY[i]) <= circleRadius * circleRadius) {
+            isDragging = true;
+            whichDragging = i;
+            offsetX = newX - oriX[i];
+            offsetY = newY - oriY[i];
+        }
     }
-    if (isDragging) {
-        x = newX;
-        y = newY;
-        showCircle();
-    }
-}
-
-canvas.addEventListener("mousedown", function(event) {
-    updateCircle(true);
 });
 
 canvas.addEventListener("mousemove", function(event) {
-    updateCircle();
+    const rect = canvas.getBoundingClientRect();
+    newX = event.clientX - rect.left;
+    newY = event.clientY - rect.top;
+    if (isDragging) {
+        oriX[whichDragging] = newX - offsetX;
+        oriY[whichDragging] = newY - offsetY;
+        showCircles();
+    }
 });
 
 canvas.addEventListener("mouseup", function(event) {
