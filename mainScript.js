9638903ab57e0711e8b22c7882ff14ec37607990
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2023-04-21 19:30:22 +0800

    Update mainScript.js
    Modified by ChatGPT.

--- mainScript.js
+++ mainScript.js
@@ -1,15 +1,17 @@
+javascript
+Copy
 const canvas = document.createElement("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 document.body.appendChild(canvas);
 
 const context = canvas.getContext("2d");
-const radius = 60;
-var x = canvas.width / 2;
-var y = canvas.height / 2;
-var isDragging = false;
-var offsetX = 0;
-var offsetY = 0;
+const radius = 50;
+let x = canvas.width / 2;
+let y = canvas.height / 2;
+let isDragging = false;
+let offsetX = 0;
+let offsetY = 0;
 
 function animateCircle() {
     context.clearRect(0, 0, canvas.width, canvas.height);
@@ -24,9 +26,9 @@ animateCircle();
 
 canvas.addEventListener("mousedown", function(event) {
     const rect = canvas.getBoundingClientRect();
-    offsetX = event.clientX - rect.left;
-    offsetY = event.clientY - rect.top;
-    if (Math.sqrt(Math.pow(offsetX - x, 2) + Math.pow(offsetY - y, 2)) <= radius) {
+    offsetX = event.clientX - rect.left - x;
+    offsetY = event.clientY - rect.top - y;
+    if (Math.sqrt(Math.pow(offsetX, 2) + Math.pow(offsetY, 2)) <= radius) {
         isDragging = true;
     }
 });
