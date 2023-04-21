6fe7591bf665c8bcf387a04ebba9a9cf1ffc7392
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2023-04-21 19:53:35 +0800

    Update mainScript.js

--- mainScript.js
+++ mainScript.js
@@ -2,20 +2,21 @@ const canvas = document.createElement("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 document.body.appendChild(canvas);
+element.style.zIndex = 3;
 
 const context = canvas.getContext("2d");
-const radius = 50;
-let x = canvas.width / 2;
-let y = canvas.height / 2;
-let isDragging = false;
-let offsetX = 0;
-let offsetY = 0;
+const radius = 40;
+var x = canvas.width / 2;
+var y = canvas.height / 2;
+var isDragging = false;
+var newX;
+var newY;
 
 function animateCircle() {
     context.clearRect(0, 0, canvas.width, canvas.height);
     context.beginPath();
     context.arc(x, y, radius, 0, Math.PI * 2);
-    context.fillStyle = "blue";
+    context.fillStyle = "green";
     context.fill();
     requestAnimationFrame(animateCircle);
 }
@@ -24,9 +25,9 @@ animateCircle();
 
 canvas.addEventListener("mousedown", function(event) {
     const rect = canvas.getBoundingClientRect();
-    offsetX = event.clientX - rect.left - x;
-    offsetY = event.clientY - rect.top - y;
-    if (Math.sqrt(Math.pow(offsetX, 2) + Math.pow(offsetY, 2)) <= radius) {
+    newX = event.clientX - rect.left;
+    newY = event.clientY - rect.top;
+    if ((newX - x, 2) * (newX - x, 2) + (newY - y, 2) * (newY - y, 2) <= radius * radius) {
         isDragging = true;
     }
 });
@@ -34,8 +35,11 @@ canvas.addEventListener("mousedown", function(event) {
 canvas.addEventListener("mousemove", function(event) {
     if (isDragging) {
         const rect = canvas.getBoundingClientRect();
-        x = event.clientX - rect.left - offsetX;
-        y = event.clientY - rect.top - offsetY;
+        x = newX;
+        y = newY;
+        animateCircle();
+        console.log("Mouse is moving!");
+        console.log("Mouse position: ", event.clientX, event.clientY);
     }
 });
 
